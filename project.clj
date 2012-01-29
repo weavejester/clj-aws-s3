2d0990c16b1416af81a3ace81c851b53108c947c
James Reeves <jreeves@weavejester.com>
2012-01-29 18:36:32 +0000

    Added Codox for API docs

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-aws-s3 "0.1.0"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.0"]])
+                 [com.amazonaws/aws-java-sdk "1.3.0"]]
+  :dev-dependencies [[codox "0.3.4"]])
