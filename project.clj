969aa6c019e13b68f0386d1b9c97d7229c99bb20
James Reeves <jreeves@weavejester.com>
2012-04-04 14:59:03 +0100

    Updated AWS SDK to 1.3.6

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-aws-s3 "0.2.1"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.0"]]
+                 [com.amazonaws/aws-java-sdk "1.3.6"]]
   :plugins [[codox "0.6.1"]])
