f055af7905a6620c7d7d2dc2df7e7d42c884c0dc
James Reeves <jreeves@weavejester.com>
2012-04-04 14:57:36 +0100

    Updated Codox to 0.6.1

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.3.0"]]
-  :dev-dependencies [[codox "0.4.0"]])
+  :plugins [[codox "0.6.1"]])
