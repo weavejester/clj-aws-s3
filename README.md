f2d24c5d1bc61d4d854e9a4eb613a58613bff21b
James Reeves <jreeves@weavejester.com>
2012-04-29 18:14:44 +0100

    Released 0.3.1

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ and update the access control lists (ACLs) for buckets and objects.
 
 Add the following dependency to your `project.clj` file:
 
-    [clj-aws-s3 "0.3.0"]
+    [clj-aws-s3 "0.3.1"]
 
 ## Example
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-s3 "0.3.0"
+(defproject clj-aws-s3 "0.3.1"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.3.6"]]
