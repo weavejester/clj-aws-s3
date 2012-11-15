7e66a58846351d29f65fa6ff0ac0a247b34722ba
James Reeves <jreeves@weavejester.com>
2012-11-15 13:47:32 -0500

    Added generate-presigned-url function

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.backstopsolutions/clj-aws-s3 "0.3.3-SNAPSHOT"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.18"]]
+                 [com.amazonaws/aws-java-sdk "1.3.18"]
+                 [clj-time "0.4.4"]]
   :plugins [[codox "0.6.1"]])
