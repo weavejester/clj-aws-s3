04fb2f90a2270b07bca7493cfea149844412d01e
James Reeves <jreeves@weavejester.com>
2013-04-22 17:50:34 +0100

    Released 0.3.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-aws-s3 "0.3.4"
+(defproject clj-aws-s3 "0.3.5"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2.1"]
