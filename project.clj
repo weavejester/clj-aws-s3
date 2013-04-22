e6cedeb124fb7da269781cfbc81aa235c7997e6c
James Reeves <jreeves@weavejester.com>
2013-04-22 17:48:35 +0100

    Updated clj-time version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2.1"]
-                 [clj-time "0.4.4"]]
+                 [clj-time "0.5.0"]]
   :plugins [[codox "0.6.4"]])
