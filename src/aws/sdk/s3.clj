5ba49e08a3c3e439f05577ebb12b53ac970b942e
camerazn <christopheragocs@cagocs.backstop.local>
2013-11-22 13:20:28 -0600

    blowing away whitespace changes

--- src/aws/sdk/s3.clj
+++ src/aws/sdk/s3.clj
@@ -53,12 +53,12 @@ Map may also contain the configuration keys :conn-timeout,
 :socket-timeout, :max-conns, and :max-retries."
   [cred]
   (let [client-configuration (ClientConfiguration.)]
-    (when-let [proxy-host (:proxy-host cred)]
-      (.setProxyHost client-configuration proxy-host))
-    (when-let [proxy-port (:proxy-port cred)]
-      (.setProxyPort client-configuration proxy-port))
-    (when-let [protocol-str (:protocol-str cred)]
-      (if (= (clojure.string/upper-case protocol-str) "HTTP")
+    (when-let [proxyHost (:proxyHost cred)]
+      (.setProxyHost client-configuration proxyHost))
+    (when-let [proxyPort (:proxyPort cred)]
+      (.setProxyPort client-configuration proxyPort))
+    (when-let [protocolStr (:protocolStr cred)]
+      (if (= (clojure.string/upper-case protocolStr) "HTTP")
         (.setProtocol client-configuration Protocol/HTTP)
         (.setProtocol client-configuration Protocol/HTTPS)))
     (when-let [conn-timeout (:conn-timeout cred)]
@@ -229,7 +229,11 @@ Map may also contain the configuration keys :conn-timeout,
 
 (defn- upload-part
   [{cred :cred bucket :bucket key :key upload-id :upload-id
+<<<<<<< HEAD
     part-size :part-size offset :offset ^java.io.File file :file}]
+=======
+    part-size :part-size offset :offset ^java.io.File file :file}] 
+>>>>>>> 7721e79... blowing away whitespace changes
   (.getPartETag
    (.uploadPart
     (s3-client cred)
@@ -626,4 +630,7 @@ Map may also contain the configuration keys :conn-timeout,
   See update-bucket-acl."
   [grantee permission]
   #(disj % {:grantee grantee :permission permission}))
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7721e79... blowing away whitespace changes
