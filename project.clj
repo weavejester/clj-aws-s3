050d981a8f59a85b0d885d21e9999d0e5d4e3621
Colin Cross <ccross@backstopsolutions.com>
2014-01-31 16:30:53 -0600

    Allow for publishing to Backstop's local Ivy repo
    Except for the version bump, this is only relevant to Backstop. We
    shouldn't put any of the rest of this into a pull request.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject clj-aws-s3 "0.3.7"
+(defproject clj-aws-s3 "0.3.8"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2.1"]
                  [clj-time "0.5.0"]]
-  :plugins [[codox "0.6.4"]])
+  :plugins [[codox "0.6.4"]
+            [com.backstopsolutions/bsg-lein-publish "0.0.6"]]
+  :repositories [["backstop" "http://ivy.backstopsolutions"]])
