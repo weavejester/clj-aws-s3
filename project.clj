68a720048cd48b595e730dcd59484156b6e53183
Peter Gillard-Moss <peter@gillardmoss.me.uk>
2014-02-07 16:29:53 +0000

    Added update-bucket-policy function

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2.1"]
-                 [clj-time "0.5.0"]]
+                 [clj-time "0.5.0"]
+                 [cheshire "5.3.1"]]
   :plugins [[codox "0.6.4"]])
