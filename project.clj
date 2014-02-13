efadecf2d1859b5c325a647b8d1e3e481df91b56
James Reeves <jreeves@weavejester.com>
2014-02-13 12:48:41 +0000

    Added :url and :license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-aws-s3 "0.3.7"
-  :description "Clojure Amazon S3 library."
+  :description "Clojure Amazon S3 library"
+  :url "https://github.com/weavejester/clj-aws-s3"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2.1"]
                  [clj-time "0.5.0"]]
