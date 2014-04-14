cc3cfc035c0409ca2bb07a68be30715aa86c81a3
Devin Walters <devinw@gmail.com>
2014-04-14 15:49:02 -0500

    Update project dependencies
    - Bump clj-time ("0.5.0" to "0.6.0")
    - Bump aws-java-sdk ("1.6.12" to "1.7.5")
    - Bump codox ("0.6.6" to "0.6.7")

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.6.12"]
-                 [clj-time "0.5.0"]]
-  :plugins [[codox "0.6.6"]])
+                 [com.amazonaws/aws-java-sdk "1.7.5"]
+                 [clj-time "0.6.0"]]
+  :plugins [[codox "0.6.7"]])
