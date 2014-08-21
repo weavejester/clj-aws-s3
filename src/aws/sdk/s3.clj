718144be677ab0fd12f3b3b2c0914e5ffdcb289e
Tom Savage <tcsavage@gmail.com>
2014-08-21 15:00:59 +0100

    Added ability for s3-client* to use default credentials provider in-place of missing keys

--- src/aws/sdk/s3.clj
+++ src/aws/sdk/s3.clj
@@ -14,6 +14,7 @@
             [clojure.walk :as walk])
   (:import com.amazonaws.auth.BasicAWSCredentials
            com.amazonaws.auth.BasicSessionCredentials
+           com.amazonaws.auth.DefaultAWSCredentialsProviderChain
            com.amazonaws.services.s3.AmazonS3Client
            com.amazonaws.AmazonServiceException
            com.amazonaws.ClientConfiguration
@@ -73,10 +74,11 @@
     (when-let [proxy-workstation (get-in cred [:proxy :workstation])]
       (.setProxyWorkstation client-configuration proxy-workstation))
     (let [aws-creds
-          (if (:token cred)
-            (BasicSessionCredentials. (:access-key cred) (:secret-key cred) (:token cred))
-            (BasicAWSCredentials. (:access-key cred) (:secret-key cred)))
-
+          (if (and (:access-key cred) (:secret-key cred))
+            (if (:token cred)
+              (BasicSessionCredentials. (:access-key cred) (:secret-key cred) (:token cred))
+              (BasicAWSCredentials. (:access-key cred) (:secret-key cred)))
+            (DefaultAWSCredentialsProviderChain.))
           client (AmazonS3Client. aws-creds client-configuration)]
       (when-let [endpoint (:endpoint cred)]
         (.setEndpoint client endpoint))
